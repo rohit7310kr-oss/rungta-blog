7e3d6cf2466036c5426a405322fcc4c3e5d28c0d
rohit <rohit7310kr@gmail.com>
2026-03-01 10:42:31 +0530

    testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 import "./App.css";
-import Login from "./Auth/Login";
-import Signup from "./Auth/Signup";
+// import Login from "./Auth/Login";
+// import Signup from "./Auth/Signup";
 import Home from "./component/Home";
-import CreatePost from "./component/CreatePost";
-import Profile from "./component/Profile";
+// import CreatePost from "./component/CreatePost";
+// import Profile from "./component/Profile";
 
 function App() {
   return (
